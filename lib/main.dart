c614394cb123dc22dd210cfed212f201f674fd0e
smjoy222 <joy15-5777@diu.edu.bd>
2025-02-25 11:52:30 +0600

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,180 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const Lab06());
 }
 
-class MyApp extends StatelessWidget {
+class Lab06 extends StatelessWidget {
+  const Lab06({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ShoppingCartScreen(),
-    );
-  }
-}
-
-class ShoppingCartScreen extends StatelessWidget {
-  final List<CartItem> cartItems = [
-    CartItem("Apple W-series 6", "₦ 45,000", "assets/watch.png", "Size: 35"),
-    CartItem("Siberia 800", "₦ 45,000", "assets/headphones.png", "Size: M"),
-    CartItem("Lycra Men’s shirt", "₦ 45,000", "assets/shirt.png", "Size: 5"),
-    CartItem("Nike/L v Airforce 1", "₦ 45,000", "assets/shoes.png", "Size: 42"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {},
-        ),
-        title: Text("My cart", style: TextStyle(color: Colors.black)),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.all(10),
-              itemCount: cartItems.length,
-              itemBuilder: (context, index) {
-                return CartItemCard(cartItems[index]);
-              },
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Divider(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Total",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    Text("₦ 180,000",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.orange)),
-                  ],
-                ),
-                SizedBox(height: 15),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                    ),
-                    onPressed: () {},
-                    child: Text("Buy Now",
-                        style: TextStyle(fontSize: 18, color: Colors.white)),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CartItem {
-  final String title;
-  final String price;
-  final String imagePath;
-  final String size;
-
-  CartItem(this.title, this.price, this.imagePath, this.size);
-}
-
-class CartItemCard extends StatelessWidget {
-  final CartItem item;
-
-  CartItemCard(this.item);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Row(
+      title: 'Advanced UI',
+      home: Scaffold(
+        drawer: Drawer(
+            child: ListView(
           children: [
-            Image.asset(item.imagePath,
-                width: 60, height: 60, fit: BoxFit.cover),
-            SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            DrawerHeader(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(item.title,
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 5),
-                  Text(item.price,
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black)),
-                  Text(item.size,
-                      style: TextStyle(fontSize: 12, color: Colors.grey)),
+                  Icon(
+                    Icons.access_alarm_outlined,
+                    size: 60,
+                    color: Colors.blue,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Icon(Icons.closed_caption_rounded),
+                  ),
                 ],
               ),
             ),
-            QuantitySelector(),
+            ListTile(
+              leading: Icon(Icons.people_alt_rounded),
+              title: Text("Contacts"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Contacts"),
+              onTap: () {},
+            ),
           ],
+        )),
+        appBar: AppBar(
+          title: Text("Advance UI"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                  "https://images.unsplash.com/photo-1635805737707-575885ab0820?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+              Image.network(
+                  "https://images.unsplash.com/photo-1635805739892-ab7b431400f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class QuantitySelector extends StatefulWidget {
-  @override
-  _QuantitySelectorState createState() => _QuantitySelectorState();
-}
-
-class _QuantitySelectorState extends State<QuantitySelector> {
-  int quantity = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          icon: Icon(Icons.remove, color: Colors.orange),
-          onPressed: () {
-            if (quantity > 1) {
-              setState(() {
-                quantity--;
-              });
-            }
-          },
-        ),
-        Text("$quantity", style: TextStyle(fontSize: 16)),
-        IconButton(
-          icon: Icon(Icons.add, color: Colors.orange),
-          onPressed: () {
-            setState(() {
-              quantity++;
-            });
-          },
-        ),
-      ],
     );
   }
 }
